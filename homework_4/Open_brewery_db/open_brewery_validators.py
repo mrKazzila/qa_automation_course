3884965f484a391a579d14b55152330f1178e0de
Kazzila <mrkazila@yandex.ru>
2022-05-03 23:42:09 +0300

    [homework] Test for rest api: https://www.openbrewerydb.org/

---
+++ homework_4/Open_brewery_db/open_brewery_validators.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from pydantic import BaseModel
+
+
+class SingleBreweryValidator(BaseModel):
+    id: str
+    brewery_type: str
+    name: str
+    street: Union[None, str]
+    address_2: Union[None, str]
+    address_3: Union[None, str]
+    city: str
+    state: str
+    county_province: Union[None, str]
+    postal_code: Union[None, str]
+    country: str
+    longitude: Union[None, str]
+    latitude: Union[None, str]
+    phone: str
+    website_url: str
+    updated_at: str
+    created_at: str
