99196b9fd74f587119ac9145cafbd3f22e290216
Kazzila <mrkazila@yandex.ru>
2022-05-03 23:42:39 +0300

    [homework] Test for rest api: https://jsonplaceholder.typicode.com/

---
+++ homework_4/Jsonplaceholder/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.fixture
+def base_url():
+    return 'https://jsonplaceholder.typicode.com'
+
+
+@pytest.fixture
+def post_data():
+    return dict(
+        userId=1,
+        title='foo',
+        body='bar'
+    )
+
+
+@pytest.fixture
+def header():
+    return {'Content-type': 'application/json; charset=UTF-8'}
