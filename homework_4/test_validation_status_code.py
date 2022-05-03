7cf1ed7c393bf5c4642c75a9123d97b143341824
Kazzila <mrkazila@yandex.ru>
2022-05-03 23:44:14 +0300

    [homework] Test for check status code with pytest.addoption

---
+++ homework_4/test_validation_status_code.py
@@ -0,0 +1,17 @@
+import pytest
+import requests
+
+
+@pytest.fixture
+def base_url(request):
+    return request.config.getoption("--url")
+
+
+@pytest.fixture
+def status_code(request):
+    return int(request.config.getoption("--status_code"))
+
+
+def test_status_code_for_url(base_url, status_code):
+    response = requests.get(url=base_url)
+    assert response.status_code == status_code
