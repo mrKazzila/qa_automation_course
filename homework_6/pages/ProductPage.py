cb1b36b688ef07680d291ef345f647c9be64a065
Kazzila <mrkazila@yandex.ru>
2022-09-08 22:20:13 +0300

    [homework] Add pages and elements

---
+++ homework_6/pages/ProductPage.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+from homework_6.pages.BasePage import BasePage
+
+
+class ProductPage(BasePage):
+    ADD_URL: str = r"/desktops"
+    TITLE: str = 'Desktops'
+
+    LOGO: tuple = (By.CSS_SELECTOR, "img[title='Your Store']")
+    GOODS_GROUPS: tuple = (By.CSS_SELECTOR, "div[class='list-group']")
+    DESCRIPTION_FIELD: tuple = (By.CSS_SELECTOR, "div[class='col-sm-10']")
+    IMAGE_FIELD: tuple = (By.CSS_SELECTOR, "div[class='col-sm-2']")
