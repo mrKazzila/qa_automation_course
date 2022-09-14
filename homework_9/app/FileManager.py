2a686d55e7670a0e12258aa45447466a8c7622c8
Kazzila <mrkazila@yandex.ru>
2022-09-14 21:08:26 +0300

    [homework] Add parser for access.log

---
+++ homework_9/app/FileManager.py
@@ -0,0 +1,38 @@
+import json
+import os
+from datetime import datetime
+from typing import NoReturn, Tuple
+
+
+class FileManager:
+
+    @staticmethod
+    def check_file_exist(file_path: str) -> NoReturn:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"File or dir {file_path} not found!")
+
+    @staticmethod
+    def get_path_with_log_files(path: str) -> Tuple[str, str]:
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in files:
+                if name.endswith('.log'):
+                    yield os.path.normpath(os.path.join(root, name)), name
+
+    @staticmethod
+    def parse_log_file(log_file: str) -> str:
+        print(f'Work with file: {log_file}')
+        with open(log_file, 'r') as log_f:
+            for line in log_f:
+                yield line.replace("\n", '').strip()
+
+    @staticmethod
+    def create_result_file_name() -> str:
+        date_format = datetime.now().strftime("%d-%m-%Y-%H-%M")
+        return os.path.join(os.getcwd(), f'parse_result_{date_format}.json')
+
+    @staticmethod
+    def writer(output_file: str, log_data) -> NoReturn:
+        with open(output_file, 'w', encoding='utf-8') as json_file:
+            print(json.dumps(log_data, indent=3, ensure_ascii=False))
+            json.dump(log_data, json_file, indent=3, ensure_ascii=False)
+        print(f"The json file was created, the path for the file: {output_file}")
---
+++ homework_9/app/helpers.py
@@ -0,0 +1,96 @@
+import re
+from collections import Counter
+from typing import Optional, Union
+
+from FileManager import FileManager
+from data import Data, methods
+
+
+def check_is_file(log_path: str) -> bool:
+    return True if log_path.endswith('.log') else False
+
+
+def result_creator(data_count: dict, method_count: dict,
+                   popular_ip: dict, longest_requests: dict) -> dict:
+    return {
+        **data_count,
+        **method_count,
+        **popular_ip,
+        **longest_requests
+    }
+
+
+def parse_dir(log_file: str) -> dict:
+    result = {}
+    for path, file_name in FileManager.get_path_with_log_files(log_file):
+        result.update({file_name: file_parse(path)})
+    return result
+
+
+def file_parse(log_file: str) -> dict:
+    parse_data = {
+        line_id: parse_line(line)
+        for line_id, line in enumerate(FileManager.parse_log_file(log_file), start=1)
+    }
+    return result_creator(
+        data_count=dict(count=len(parse_data)),
+        method_count=get_method_count(data=parse_data),
+        popular_ip=get_ip_count(data=parse_data),
+        longest_requests=get_longest_requests(data=parse_data)
+    )
+
+
+def parse_line(line: str) -> Data:
+    REGEX_METHOD = re.compile(r'(POST|GET|PUT|DELETE|HEAD|CONNECT|OPTIONS|TRACE|PATCH)')
+    REGEX_IP_ADDRESS = re.compile(r'(\d{1,3}\.){3}\d{1,3}')
+    REGEX_URL = re.compile(r'"(http[s]?:\/\/.+)" "')
+    REGEX_DATE_TIME = re.compile(r'\[(.+) \+\d{4}\]')
+    REGEX_DURATION = re.compile(r'\d+$')
+
+    return Data(
+        method_=get_method_match(line=line, regex_=REGEX_METHOD),
+        ip_=get_ip_address_match(line=line, regex_=REGEX_IP_ADDRESS),
+        url_=get_url_match(line=line, regex_=REGEX_URL),
+        date_time_=get_time_match(line=line, regex_=REGEX_DATE_TIME),
+        duration_=duration_match(line=line, regex_=REGEX_DURATION)
+    )
+
+
+def get_method_match(line: str, regex_: re) -> str:
+    match = regex_.search(line)
+    return match.group(0) if match else '-'
+
+
+def get_ip_address_match(line: str, regex_: re) -> str:
+    match = regex_.search(line)
+    return match.group(0) if match else '-'
+
+
+def get_url_match(line: str, regex_: re) -> str:
+    match = regex_.search(line)
+    return match.group(1) if match else '-'
+
+
+def duration_match(line: str, regex_: re) -> Optional[Union[int, str]]:
+    match = regex_.search(line)
+    return int(match.group(0)) if match else '-'
+
+
+def get_time_match(line: str, regex_: re) -> str:
+    match = regex_.search(line)
+    return match.group(1) if match else '-'
+
+
+def get_method_count(data: dict) -> dict:
+    for info in data.values():
+        methods[info.method_] = methods.get(info.method_) + 1
+    return {"methods": methods}
+
+
+def get_ip_count(data: dict, top_: int = 3) -> dict:
+    ip_s = Counter([info.ip_ for info in data.values()])
+    return {f"top_{top_}_ipS": sorted(ip_s.items(), key=lambda x: -x[1])[:top_]}
+
+
+def get_longest_requests(data: dict, top_: int = 10) -> dict:
+    return {f"top_{top_}_duration": sorted(data.values(), key=lambda x: -x.duration_)[:top_]}
